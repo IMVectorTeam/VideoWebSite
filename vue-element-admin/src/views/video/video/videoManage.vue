86b0f634367f5340f73851b5046a921c99fb8088
Kun Jia <42566721+tajkun@users.noreply.github.com>
2019-01-03 17:47:45 +0800

    videoManage

--- vue-element-admin/src/views/video/video/videoManage.vue
+++ vue-element-admin/src/views/video/video/videoManage.vue
@@ -1,5 +1,39 @@
 <template>
-  <div/>
+  <div class="app-container">
+    <el-table
+      :data="videoList"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="名字"
+        align="center"
+        width="180"/>
+      <el-table-column
+        prop="id"
+        label="id"
+        align="center"
+        width="180"/>
+      <el-table-column
+        prop="time"
+        label="时间"
+        align="center"
+        width="180"/>
+      <el-table-column
+        prop="category"
+        label="类别"
+        align="center"
+        width="180"/>
+      <el-table-column
+        align="center"
+        label="操作">
+        <template slot-scope="scope">
+          <el-button type="danger" @click="handleDelete(scope.row,scope.$index)">删除</el-button>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+  </div>
 </template>
 
 <script>
